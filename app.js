3272b188d3be044d15b991413660631cc534fac3
Abdifatah Basher <56160528+abasher423@users.noreply.github.com>
2020-12-07 06:30:15 +0000

    added update grid function

--- app.js
+++ app.js
@@ -1,43 +1,40 @@
-const canvas = document.querySelector('#canvas')
+// Variable to select the outer (main) grid
+const grid = document.querySelector('#grid')
+
+// Variables to select the inputs
 const blackBtn = document.querySelector('.black')
 const resetBtn = document.querySelector('.reset')
 const rgbBtn = document.querySelector('.rgb')
+const colorWell = document.querySelector('#col')
+const slider = document.querySelector('#slider')
 
-const divColor = null
-CreateCanvas(16)
-const div = document.querySelectorAll('#canvas div')
+// Colors array containing colors from the rainbow
+const colors = [
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'indigo',
+    'violet'
+]
 
-function CreateCanvas(num){
-    canvas.style.gridTemplateColumns = `repeat(${num},1fr)`
-    canvas.style.gridTemplateRows = `repeat(${num},1fr)`
-    canvas.setAttribute('gridTemplateRows', num)
-    for (let i=0; i<num*num; i++){
+// function creates a grid depending on the size it recieves
+function CreateGrid(gridSize){
+    grid.style.gridTemplateColumns = `repeat(${gridSize},1fr)`
+    grid.style.gridTemplateRows = `repeat(${gridSize},1fr)`
+    for (let i=0; i<gridSize*gridSize; i++){
         const div = document.createElement('div')
-        canvas.appendChild(div)
-    }
+        grid.appendChild(div)
+    }    
 }
 
-function SetSquareColor(divColor){
-    div.forEach(el => el.addEventListener('mouseenter', ()=>{
-        el.style.backgroundColor = divColor
-     }))
+// function which updates the number of cells in the grid
+function UpdateGrid(){
+    document.querySelector('#sliderLabel').innerHTML = slider.value
+    RemoveAllChildNodes(grid)
+    CreateGrid(slider.value)
+    Reset()
+    SetSquareColor('white')
 }
 
-blackBtn.addEventListener('click', ()=>{
-    SetSquareColor('black')
-})
-
-rgbBtn.addEventListener('click', ()=>{
-    div.forEach(el => el.addEventListener('mouseenter', ()=>{
-        const r = Math.floor(Math.random() * 255)
-        const g = Math.floor(Math.random() * 255)
-        const b = Math.floor(Math.random() * 255)
-        el.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
-     }))
-})
-
-resetBtn.addEventListener('click', ()=>{
-    for(let i=0; i<div.length; i++){
-        div[i].style.backgroundColor = 'white'
-    }
-})
