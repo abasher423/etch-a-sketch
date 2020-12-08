e0e8c8c722f969676ef55f915e760c99a11804f8
Abdifatah Basher <56160528+abasher423@users.noreply.github.com>
2020-12-08 10:39:37 +0000

    code clean up

--- app.js
+++ app.js
@@ -1,26 +1,13 @@
-// Variable to select the outer (main) grid
+// Variables to select the outer (main) grid and inputs
 const grid = document.querySelector('#grid')
-
-// Variables to select the inputs
 const blackBtn = document.querySelector('.black')
 const resetBtn = document.querySelector('.reset')
 const rgbBtn = document.querySelector('.rgb')
 const colorWell = document.querySelector('#col')
 const slider = document.querySelector('#slider')
 
-// Colors array containing colors from the rainbow
-const colors = [
-    'red',
-    'orange',
-    'yellow',
-    'green',
-    'blue',
-    'indigo',
-    'violet'
-]
-
 // function creates a grid depending on the size it recieves
-function CreateGrid(gridSize){
+const CreateGrid = gridSize => {
     grid.style.gridTemplateColumns = `repeat(${gridSize},1fr)`
     grid.style.gridTemplateRows = `repeat(${gridSize},1fr)`
     for (let i=0; i<gridSize*gridSize; i++){
@@ -30,48 +17,52 @@ function CreateGrid(gridSize){
 }
 
 // function which updates the number of cells in the grid
-function UpdateGrid(){
+const UpdateGrid = () => {
     document.querySelector('#sliderLabel').innerHTML = slider.value
     RemoveAllChildNodes(grid)
     CreateGrid(slider.value)
     Reset()
-    SetSquareColor('white')
 }
 
 // function which removes all child nodes (used when updating grid)
-function RemoveAllChildNodes(parent){
+const RemoveAllChildNodes = parent => {
     while (parent.firstChild){
         parent.removeChild(parent.firstChild)
     }
 }
 
 // function to set the cells background color
-function SetSquareColor(divColor){
-    let cells = grid.children
-    for (let i=0; i<cells.length; i++){
-        cells[i].addEventListener('mouseenter', ()=>{
-            cells[i].style.backgroundColor = divColor
-        })
-    }
+const SetCellColor = event => {
+    document.querySelectorAll('#grid div').forEach(cell => cell.addEventListener('mouseover', ()=>{
+        if (event.target === blackBtn){
+            cell.style.backgroundColor = 'black'
+        } else if (event.target === rgbBtn){
+            cell.style.backgroundColor = Rainbow()
+        }
+    }))
 }
 
 // function assigns cells background color a random color from the array
 function Rainbow(){
-    let cells = grid.children
-    for (let i=0; i<cells.length; i++){
-        cells[i].addEventListener('mouseenter', ()=>{
-            cells[i].style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
-         })
-    }
+    const colors = [
+        'red',
+        'orange',
+        'yellow',
+        'green',
+        'blue',
+        'indigo',
+        'violet'
+    ]
+    return colors[Math.floor(Math.random() * colors.length)]
 }
 
 // function which updates or changes cells color when color picker is used
 function updateFirst(event) {
     let cells = grid.children
-        for (let i=0; i<cells.length; i++){
-            cells[i].addEventListener('mouseenter', ()=>{
-            if (cells[i]){
-                cells[i].style.backgroundColor = event.target.value
+    for (let cell of cells){
+        cell.addEventListener('mouseover', ()=>{
+            if (cell){
+                cell.style.backgroundColor = event.target.value
             }
         })
     }
@@ -79,21 +70,16 @@ function updateFirst(event) {
 
 // function to reset all cells back to white
 function Reset(){
-    let cells = grid.children
-    for (let i=0; i<cells.length; i++){
-        cells[i].style.backgroundColor = 'white'
-    }
+    document.querySelectorAll('#grid div').forEach(cell => cell.style.backgroundColor = 'white')
 }
-
-// event listeners
-blackBtn.addEventListener('click', ()=>{
-    SetSquareColor('black')
-})
-resetBtn.addEventListener('click', Reset)
-rgbBtn.addEventListener('click', Rainbow)
-colorWell.addEventListener("change", updateFirst, false);
-colorWell.addEventListener("input", updateFirst, false);
-slider.addEventListener('change', UpdateGrid)
 
 // initialises grid to 16x16
 CreateGrid(16)
+
+// event listeners
+resetBtn.addEventListener('click', Reset)
+blackBtn.addEventListener('click', SetCellColor)
+rgbBtn.addEventListener('click', SetCellColor)
+colorWell.addEventListener("change", updateFirst, false);
+colorWell.addEventListener("input", updateFirst, false);
+slider.addEventListener('change', UpdateGrid)
